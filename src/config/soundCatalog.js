eb28785afab27f835e48aa4285a10dd86c8204f1
fmscr31-tech <fmscr31@gmail.com>
2026-08-15 19:56:37 -0600

    Fix sound catalog for Ability Manager selectors

--- src/config/soundCatalog.js
+++ src/config/soundCatalog.js
@@ -1,4 +1,33 @@
 /**
- * Sound Catalog - Auto-Discovered dynamically from public/Sounds/ via Vite plugin
+ * Sound Catalog - Available MP3 assets in public/Sounds/.
+ * Keep paths stable because persisted ability/gift configurations store them.
  */
-export const AVAILABLE_SOUNDS = [];
+export const AVAILABLE_SOUNDS = [
+  "/Sounds/5-4-3-2-1-are-you-ready.mp3",
+  "/Sounds/AK 47.mp3",
+  "/Sounds/Ahh cute.mp3",
+  "/Sounds/Capibara.mp3",
+  "/Sounds/Castigados.mp3",
+  "/Sounds/Estas triste.mp3",
+  "/Sounds/Gemido.mp3",
+  "/Sounds/Grito feo.mp3",
+  "/Sounds/Kamehameha.mp3",
+  "/Sounds/La Ocasión.mp3",
+  "/Sounds/NBA punto.mp3",
+  "/Sounds/Oof.mp3",
+  "/Sounds/Papasito Mua.mp3",
+  "/Sounds/Perro riendose.mp3",
+  "/Sounds/PornHub.mp3",
+  "/Sounds/Que rico Karol G.mp3",
+  "/Sounds/Quiereme.mp3",
+  "/Sounds/Reinicio.mp3",
+  "/Sounds/Sombrero Vaquero.mp3",
+  "/Sounds/Squeaky.mp3",
+  "/Sounds/Uff Mi Amor.mp3",
+  "/Sounds/Violin triste.mp3",
+  "/Sounds/are-you-ready.mp3",
+  "/Sounds/buenos-dias-papacito.mp3",
+  "/Sounds/coconut-sfx.mp3",
+  "/Sounds/hombres-celebrando.mp3",
+  "/Sounds/mudo.mp3"
+];
