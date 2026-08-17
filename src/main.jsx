cfdabeea14470de899039ad5c64566958d8267e2
fmscr31-tech <fmscr31@gmail.com>
2026-08-17 07:34:21 -0600

    fix: load final gender team identity hard-lock

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import "./components/individual-points.css";
 import "./components/overlay/team-panel-polish.css";
 import "./components/overlay/team-gender-visual-overrides.css";
 import "./components/overlay/team-label-render-fix.css";
+import "./components/overlay/team-identity-hard-lock.css";
 import { audioManager } from "./core/audioManager";
 import "./core/chatCommandParser";
 import "./core/winBridgeWatchdog";
