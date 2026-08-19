1ea79183c360ff2af4c482adb3d642d86323a7e5
fmscr31-tech <fmscr31@gmail.com>
2026-08-18 19:02:33 -0600

    fix: load girls team card fuchsia override

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import "./components/overlay/team-gender-visual-overrides.css";
 import "./components/overlay/visual-motion-recovery.css";
 import "./components/overlay/team-identity-final.css";
 import "./components/overlay/money-gun-team-effect.css";
+import "./components/overlay/girls-team-card-fuchsia.css";
 import { audioManager } from "./core/audioManager";
 import "./core/genderTeamRegistrationBridge";
 import "./components/overlay/gender-team-join-announcement";
